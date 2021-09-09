a52cb13d3693414e479a096610f6017774ac8774
Brandon Campbell <brcampb@microsoft.com>
2021-09-09 13:54:50 -0400

    add function for removing services from AKS cluster before deploying new services

--- ml_service/pipelines/forecasting/remove_services.py
+++ ml_service/pipelines/forecasting/remove_services.py
@@ -1,9 +1,29 @@
-
-
-
-
+import azureml.core
+from azureml.core import Workspace, Datastore
+import pandas as pd
+import os
 from azureml.core import Webservice
-for webservice in Webservice.list(ws):
-    print('name:', webservice.name)
-    if "manymodels" in webservice.name:
-        Webservice(ws, name = webservice.name).delete()
+
+from ml_service.utils.env_variables import Env
+
+def main():
+
+    # set up workspace
+    # ws=Workspace.from_config()
+    # Get the variables defined in config file .env
+    e=Env()
+
+    ws = Workspace.get(
+        name=e.workspace_name,
+        subscription_id=os.environ.get("SUBSCRIPTION_ID"),
+        resource_group=os.environ.get("RG"),
+    )
+
+    for webservice in Webservice.list(ws):
+        print('name:', webservice.name)
+        if "manymodels" in webservice.name:
+            Webservice(ws, name = webservice.name).delete()
+
+
+if __name__ == "__main__":
+    main()
