7d0492c353f0c5df02b0c18c77591571b0d22120
Brandon Campbell <brcampb@microsoft.com>
2021-09-11 22:28:56 +0000

    changed procedure for authenticating to AML workspace

--- ml_service/utils/aml_workspace.py
+++ ml_service/utils/aml_workspace.py
@@ -5,18 +5,12 @@ import os
 e=Env()
 
 class Connect:
-    
-    def __init__(self, subscription_id):
-        self._subscription_id = subscription_id
-
     def authenticate(self) -> Workspace:
-        subscription_id = self._subscription_id
-        
         return Workspace.get(
             name=e.workspace_name,
-            subscription_id=subscription_id,
+            subscription_id=e.subscription_id,
             resource_group=e.resource_group
        )
     
     def authenticate_with_sp():
-        return None
+        pass
